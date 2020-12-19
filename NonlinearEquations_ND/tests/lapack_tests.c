779076c09aa4c8e86b8716c318078f2c764e9ef6
bogdan <lakatosh@phystech.edu>
2020-12-19 23:53:59 +0300

    inverse matrix loose precision rather quickly with eq_number increase

--- NonlinearEquations_ND/tests/lapack_tests.c
+++ NonlinearEquations_ND/tests/lapack_tests.c
@@ -1,67 +1,98 @@
 ﻿#include <stdio.h>
-#include "cblas.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 #include "lapacke.h"
 
-// inplace inverse n x n matrix A.
-// matrix A is Column Major (i.e. firts line, second line ... *not* C[][] order)
-// returns:
-//   ret = 0 on success
-//   ret < 0 illegal argument value
-//   ret > 0 singular matrix
 
-lapack_int matInv(double *A, unsigned n)
-{
-    int ipiv[n+1];
-    lapack_int ret;
+void error_hendler(const int info);
+void inverse_square_matrix(double* matrix, const uint n);
+void matrix_vector_mult(double** solution, const double* inv_m, const double* rhs,
+                   const uint n);
+void randomly_fill_array(double** arr, const uint size);
+void check_solution(const double* solution, const double* lhs, const double* rhs,
+                    const uint eq_num, const double defect);
 
-    ret =  LAPACKE_dgetrf(LAPACK_COL_MAJOR,
-                          n,
-                          n,
-                          A,
-                          n,
-                          ipiv);
+void my_inverse_2x2(double** result, const double* input);
 
-    if (ret !=0)
-        return ret;
-
-
-    ret = LAPACKE_dgetri(LAPACK_COL_MAJOR,
-                       n,
-                       A,
-                       n,
-                       ipiv);
-    return ret;
+int main(){
+    srand((uint)time(NULL));
+    const double delta = 1e-10;
+    const uint eq_num = 10;
+    const uint matrix_size = eq_num*eq_num;
+    double* rhs;
+    double* lhs;
+    randomly_fill_array(&rhs, eq_num);
+    randomly_fill_array(&lhs, matrix_size);
+    double* lhs_inv = calloc(matrix_size, sizeof(*lhs_inv));
+    memcpy(lhs_inv, lhs, matrix_size*sizeof(double));
+    inverse_square_matrix(lhs_inv, eq_num);
+    double* solution;
+    matrix_vector_mult(&solution, lhs_inv, rhs, eq_num);
+    check_solution(solution, lhs, rhs, eq_num, delta);
+    return 0;
 }
 
-int main()
-{
-    /*double A[] = {
-        0.378589,   0.971711,   0.016087,   0.037668,   0.312398,
-        0.756377,   0.345708,   0.922947,   0.846671,   0.856103,
-        0.732510,   0.108942,   0.476969,   0.398254,   0.507045,
-        0.162608,   0.227770,   0.533074,   0.807075,   0.180335,
-        0.517006,   0.315992,   0.914848,   0.460825,   0.731980
-    };*/
 
-    double A[] = {
-      1.0, 0.0, 0.0, 0.0, 0.0,
-      0.0, 1.0, 0.0, 0.0, 0.0,
-      0.0, 0.0, 1.0, 0.0, 0.0,
-      0.0, 0.0, 0.0, 1.0, 0.0,
-      0.0, 0.0, 0.0, 0.0, 1.0
-    };
-
-    for (int i=0; i<25; i++) {
-        if ((i%5) == 0) putchar('\n');
-        printf("%+12.8f ",A[i]);
+void error_hendler(const int info){
+    if(info!=0){
+        fprintf(stderr, "Something went wrong!");
+        exit(EXIT_FAILURE);
     }
-    putchar('\n');
+}
 
-    matInv(A,5);
 
-    for (int i=0; i<25; i++) {
-        if ((i%5) == 0) putchar('\n');
-        printf("%+12.8f ",A[i]);
+void inverse_square_matrix(double* matrix, const uint n){
+    int ipiv[n];
+    int info = LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, matrix, n, ipiv);
+    error_hendler(info);
+    info = LAPACKE_dgetri(LAPACK_ROW_MAJOR, n, matrix, n, ipiv);
+    error_hendler(info);
+}
+
+
+void matrix_vector_mult(double** solution, const double* matrix, const double* vec,
+                   const uint n){
+    double* res = calloc(n, sizeof(*res));
+    for(uint i=0; i<n; i++){
+        res[i] = 0;
+        for(uint j=0; j<n; j++){
+            res[i] += matrix[n*i+j]*vec[j];
+        }
     }
-    putchar('\n');
+    *solution = res;
+}
+
+
+void randomly_fill_array(double** arr, const uint size){
+    double* tmp_arr = calloc(size, sizeof(*tmp_arr));
+    for(uint i=0; i<size; i++){
+        tmp_arr[i] = 1.0*rand()/RAND_MAX;
+    }
+    *arr = tmp_arr;
+}
+
+void check_solution(const double* solution, const double* lhs, const double* rhs,
+                    const uint eq_num, const double delta){
+    double* res;
+    matrix_vector_mult(&res, lhs, solution, eq_num);
+    for(uint i=0; i<eq_num; i++){
+        double defect = fabs(rhs[i] - res[i]);
+        if(defect>delta){
+            fprintf(stderr, "Row %i has incorrect result : defect = %.6e\n", i, defect);
+            exit(EXIT_FAILURE);
+        }
+    }
+    printf("Check was succesfull!\n");
+}
+
+void my_inverse_2x2(double** result, const double* input){
+    double det = input[0]*input[3]-input[1]*input[2];
+    double* tmp = calloc(4, sizeof(*tmp));
+    tmp[0] = input[3]/det;
+    tmp[1] = -input[1]/det;
+    tmp[2] = -input[2]/det;
+    tmp[3] = input[0]/det;
+    *result = tmp;
 }
